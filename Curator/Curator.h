85d3829c85cc64648e5d85d52819d5c6f263e990
Yanzhi Wang <yzwang.nj@gmail.com>
2016-09-16 18:53:28 +0800

    init Curator
    generated from Xcode 8 Cocoa Touch Framework template
    deployment target set to iOS 8

---
+++ Curator/Curator.h
@@ -0,0 +1,11 @@
+@import Foundation;
+
+//! Project version number for Curator.
+FOUNDATION_EXPORT double CuratorVersionNumber;
+
+//! Project version string for Curator.
+FOUNDATION_EXPORT const unsigned char CuratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Curator/PublicHeader.h>
+
+
