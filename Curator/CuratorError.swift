83309f17c5ea4c3a715f1fff76e50e30461251ea
Yanzhi Wang <yzwang.nj@gmail.com>
2016-09-20 20:33:58 +0800

    delete unused unableToCreateDirectory error

--- Curator/CuratorError.swift
+++ Curator/CuratorError.swift
@@ -3,7 +3,6 @@ extension Curator {
         case invalidLocation(CuratorLocation)
         case unableToConvertToFileURL(from: CuratorLocation)
         case unableToObtainFileReferenceURL(from: CuratorLocation)
-        case unableToCreateDirectory(for: CuratorLocation)
         case locationIsFile(CuratorLocation)
         case locationIsDirectory(CuratorLocation)
         case locationFileExist(CuratorLocation)
