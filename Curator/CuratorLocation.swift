0f031f9853f7605dd28cf6b2bfdb09fe72794cb1
Yanzhi Wang <yzwang.nj@gmail.com>
2016-09-20 20:25:28 +0800

    remove unused Location.fileExist & location.createDirectory

--- Curator/CuratorLocation.swift
+++ Curator/CuratorLocation.swift
@@ -3,23 +3,6 @@ public protocol CuratorLocation {
 }
 
 extension CuratorLocation {
-    typealias FileExistResult = (fileExist: Bool, isDirectory: Bool)
-    func fileExist() throws -> FileExistResult {
-        let url = try self.asURL()
-        guard url.isFileURL else {
-            throw Curator.Error.unableToConvertToFileURL(from: self)
-        }
-        
-        let path = url.path
-        guard path.isEmpty == false else {
-            throw Curator.Error.invalidLocation(self)
-        }
-        
-        var isDirectory: ObjCBool = false
-        let fileExist = CuratorFileManager.fileExists(atPath: path, isDirectory: &isDirectory)
-        return (fileExist, isDirectory.boolValue)
-    }
-    
     func fileReferenceURL() throws -> URL {
         let url = try self.asURL() as NSURL
         
@@ -28,27 +11,6 @@ extension CuratorLocation {
         }
         
         return fileReferenceURL as URL
-    }
-    
-    func createDirectory() throws {
-        let url = try self.asURL()
-        let directoryURL = url.crt.getDirectoryURL()
-        
-        let directoryExistResult = try directoryURL.fileExist()
-        
-        if directoryExistResult.fileExist {
-            if directoryExistResult.isDirectory {
-                return
-            }
-            else {
-                throw Curator.Error.unableToCreateDirectory(for: self)
-            }
-        }
-        
-        try CuratorFileManager.createDirectory(
-            at: directoryURL,
-            withIntermediateDirectories: true
-        )
     }
     
     func standardizedFileURL() throws -> URL {
